723054218341ea6429a0dfb4ec66b8d6f6f2591a
柴田　大介 <dshibata@yahoo-corp.jp>
2017-11-29 04:25:10 +0900

    ダミーデータを追加

--- FileViewerSample/ViewController.swift
+++ FileViewerSample/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        saveDummyData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func saveDummyData() {
+        let fileManager = FileManager.default
+        let resourceURL = Bundle.main.resourceURL
+        guard let documentDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return
+        }
+        
+        // 画像ダミーデータを保存
+        let imageDirectoryURL = documentDirectoryURL.appendingPathComponent("images")
+        if !fileManager.fileExists(atPath: imageDirectoryURL.path) {
+            do {
+                try fileManager.createDirectory(atPath: imageDirectoryURL.path,
+                                                withIntermediateDirectories: false,
+                                                attributes: nil)
+                for i in 1..<11 {
+                    let imageName = "dummy_image\(i).jpg"
+                    if let dummyImageURL = resourceURL?.appendingPathComponent(imageName) {
+                        do {
+                            try fileManager.copyItem(atPath: dummyImageURL.path,
+                                                     toPath: documentDirectoryURL.appendingPathComponent(imageName).path)
+                        } catch {}
+                        do {
+                            try fileManager.copyItem(atPath: dummyImageURL.path,
+                                                     toPath: imageDirectoryURL.appendingPathComponent(imageName).path)
+                        } catch {}
+                    }
+                }
+            } catch {}
+        }
+        
+        // 動画ダミーデータを保存
+        let videoDirectoryURL = documentDirectoryURL.appendingPathComponent("video")
+        if !fileManager.fileExists(atPath: videoDirectoryURL.path) {
+            do {
+                try fileManager.createDirectory(atPath: videoDirectoryURL.path,
+                                                withIntermediateDirectories: false,
+                                                attributes: nil)
+                let videoName = "dummy_video1.mp4"
+                if let dummyVideoURL = resourceURL?.appendingPathComponent(videoName) {
+                    do {
+                        try fileManager.copyItem(atPath: dummyVideoURL.path,
+                                                 toPath: documentDirectoryURL.appendingPathComponent(videoName).path)
+                    } catch {
+                    }
+                }
+            } catch {}
+        }
     }
-
-
 }
 
