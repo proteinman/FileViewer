290895f6a56f41da23752127833173500f48b9d3
柴田　大介 <dshibata@yahoo-corp.jp>
2017-11-29 05:55:41 +0900

    メソッドの移動と表示の修正

--- FileViewerSample/AppDelegate.swift
+++ FileViewerSample/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        saveDummyData()
+        
         return true
     }
 
@@ -44,3 +45,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate {
+    private func saveDummyData() {
+        let fileManager = FileManager.default
+        let resourceURL = Bundle.main.resourceURL
+        guard let documentDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return
+        }
+        
+        // 画像ダミーデータを保存
+        let imageDirectoryURL = documentDirectoryURL.appendingPathComponent("images")
+        if !fileManager.fileExists(atPath: imageDirectoryURL.path) {
+            do {
+                try fileManager.createDirectory(atPath: imageDirectoryURL.path,
+                                                withIntermediateDirectories: false,
+                                                attributes: nil)
+                for i in 1..<11 {
+                    let imageName = "dummy_image\(i).jpg"
+                    if let dummyImageURL = resourceURL?.appendingPathComponent(imageName) {
+                        do {
+                            try fileManager.copyItem(atPath: dummyImageURL.path,
+                                                     toPath: documentDirectoryURL.appendingPathComponent(imageName).path)
+                        } catch {}
+                        do {
+                            try fileManager.copyItem(atPath: dummyImageURL.path,
+                                                     toPath: imageDirectoryURL.appendingPathComponent(imageName).path)
+                        } catch {}
+                    }
+                }
+            } catch {}
+        }
+        
+        // 動画ダミーデータを保存
+        let videoDirectoryURL = documentDirectoryURL.appendingPathComponent("video")
+        if !fileManager.fileExists(atPath: videoDirectoryURL.path) {
+            do {
+                try fileManager.createDirectory(atPath: videoDirectoryURL.path,
+                                                withIntermediateDirectories: false,
+                                                attributes: nil)
+                let videoName = "dummy_video1.mp4"
+                if let dummyVideoURL = resourceURL?.appendingPathComponent(videoName) {
+                    do {
+                        try fileManager.copyItem(atPath: dummyVideoURL.path,
+                                                 toPath: documentDirectoryURL.appendingPathComponent(videoName).path)
+                    } catch {
+                    }
+                }
+            } catch {}
+        }
+    }
+}
